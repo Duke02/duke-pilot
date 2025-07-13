f89a55edc904d2503745d071a24320c2524cf1ea
Trystan May <trystanmay@gmail.com>
2025-07-12 21:57:56 -0400

    Would you be surprised if I told you I spent all day without testing anything

--- duke_pilot/main.py
+++ duke_pilot/main.py
@@ -1,3 +1,8 @@
 import fastapi
 
-app: fastapi.FastAPI = fastapi.FastAPI()
+from duke_pilot.api import parser, docstore, chat
+
+app: fastapi.FastAPI = fastapi.FastAPI(root_path='/api/v1')
+app.include_router(parser.router, tags=["parser"])
+app.include_router(docstore.router, tags=["docstore"])
+app.include_router(chat.router, tags=["chat"])
